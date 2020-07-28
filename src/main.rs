9a5d62107008798f5f7a3238eb9f19f61d589917
Harsimran Singh Maan <maan.harry@gmail.com>
2020-07-28 07:16:47 -0700

    Add vector for debugging as well

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
     let message = "Hello Debug!";
     let a = add(2,3);
-    println!("{} with a={}", message,a);
+    let kind_words = vec!["Nice job", "It works"];
+    println!("{} with a={}.\n {:#?}", message,a, kind_words);
 }
 
 fn add(i:i32 ,j: i32) -> i32{
